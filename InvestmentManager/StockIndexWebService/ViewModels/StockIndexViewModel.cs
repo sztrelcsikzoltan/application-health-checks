4cc0d5c39be5af51dc6ef7c543417914a965e6c5
sztrelcsikzoltan <szzoltan@gmx.net>
2023-06-29 21:19:23 +0200

    Create DbContext and models for the databases - create StockIndexDbContext - creaate database models in the Domain folder - create similar models for the frontend MVC app in ViewModels folder - disable ImplicitUsings in the project

---
+++ InvestmentManager/StockIndexWebService/ViewModels/StockIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace StockIndexWebService.ViewModels
+{
+    public class StockIndexViewModel
+    {
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+        public string? ShortDisplayName { get; set; }
+    }
+}
