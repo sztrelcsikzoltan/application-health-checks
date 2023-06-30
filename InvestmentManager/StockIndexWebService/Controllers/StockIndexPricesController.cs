c0bf0e86ba9e4cd394ad8f2ad79428dbb6523b50
sztrelcsikzoltan <szzoltan@gmx.net>
2023-06-30 12:24:58 +0200

    Add Controllers - add StockIndexesController.cs - add StockIndexPricesController.cs - add batch file to start the app

---
+++ InvestmentManager/StockIndexWebService/Controllers/StockIndexPricesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StockIndexWebService.ViewModels;
+using System;
+using System.Linq;
+using System.Threading;
+
+namespace StockIndexWebService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockIndexPricesController : ControllerBase
+    {
+        private readonly StockIndexDbContext dataContext;
+
+        public StockIndexPricesController(StockIndexDbContext dbContext)
+        {
+            this.dataContext = dbContext;
+        }
+
+        // GET api/values
+        [HttpGet("{indexCode}")]
+        public ActionResult<StockIndexPriceViewModel> Get(String indexCode, DateTime tradeDate)
+        {
+            // Introduce some delay to simulate the delay you have in calling a service over the wire
+            Thread.Sleep(new Random().Next(250, 750));
+
+            Domain.StockIndexPrice? stockIndexPrice = this.dataContext.StockIndexPrices!
+            .Include(prop => prop.Index)
+            .Where(p => p.IndexCode == indexCode && p.TradeDate == tradeDate)
+            .FirstOrDefault();
+                        
+            if (stockIndexPrice == null)
+                return NotFound();
+
+            var model = new StockIndexPriceViewModel()
+            {
+                IndexCode = stockIndexPrice.IndexCode!,
+                IndexName = stockIndexPrice.Index!.Name!,
+                IndexShortDisplayName = stockIndexPrice.Index.ShortDisplayName!,
+                TradeDate = stockIndexPrice.TradeDate,
+                ClosePrice = stockIndexPrice.ClosePrice,
+                OpenPrice = stockIndexPrice.OpenPrice,
+                HighPrice = stockIndexPrice.HighPrice,
+                LowPrice = stockIndexPrice.LowPrice,
+                AdjustedClosePrice = stockIndexPrice.AdjustedClosePrice,
+                Volume = stockIndexPrice.Volume,
+                Change = stockIndexPrice.Change,
+                ChangePercent = stockIndexPrice.ChangePercent
+            };
+
+            return Ok(model);
+        }
+
+    }
+}
---
+++ InvestmentManager/StockIndexWebService/Controllers/StockIndexesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockIndexWebService.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace StockIndexWebService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockIndexesController : ControllerBase
+    {
+        public StockIndexesController(StockIndexDbContext dbContext)
+        {
+            this.dataContext = dbContext;
+        }
+
+        private readonly StockIndexDbContext dataContext;
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<List<StockIndexViewModel>> Get()
+        {
+            // Introduce some delay to simulate the delay you have in calling a service over the wire
+            Thread.Sleep(new Random().Next(250, 750));
+
+                var models = this.dataContext.StockIndexes!.Select(ix => new StockIndexViewModel()
+                {
+                    Code = ix.Code,
+                    Name = ix.Name,
+                    ShortDisplayName = ix.ShortDisplayName
+                }).ToList();
+            
+            if (models == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(models);
+        }
+     
+    }
+}
