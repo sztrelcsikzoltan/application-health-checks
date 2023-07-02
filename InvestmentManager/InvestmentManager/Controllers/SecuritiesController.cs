2848f3fac1f237d8e72acf9fe7d309f62665b761
sztrelcsikzoltan <szzoltan@gmx.net>
2023-07-02 18:45:00 +0200

    Create SecuritiesController and MarketsController - also create the corresponding views \Views\Markets\Index.cshtml and \Views\Securities\Index.cshtml

---
+++ InvestmentManager/InvestmentManager/Controllers/SecuritiesController.cs
@@ -0,0 +1,32 @@
+﻿using InvestmentManager.Core.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace InvestmentManager.Controllers
+{
+    public class SecuritiesController : Controller
+    {
+        public SecuritiesController(ITradeDateRepository tradeDateRepository)
+        {
+            this.tradeDateRepository = tradeDateRepository;
+        }
+
+        public ITradeDateRepository tradeDateRepository;
+
+        // GET: Securities
+        public ActionResult Index()
+        {
+            var tradeDates = tradeDateRepository.LoadTradeDates()
+                .OrderBy(d => d.Date);
+
+            return View(tradeDates);
+        }
+
+        // GET: Securities/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+    }
+}
