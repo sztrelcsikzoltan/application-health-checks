b3f35759193770392cff7abfdc6f5e9bd03eeeec
sztrelcsikzoltan <szzoltan@gmx.net>
2023-07-02 18:03:06 +0200

    Create Shared view components to view stock index values - the values are retrieved from the StockIndexWebservice project

--- InvestmentManager/InvestmentManager/Program.cs
+++ InvestmentManager/InvestmentManager/Program.cs
@@ -1,11 +1,46 @@
+using InvestmentManager.Core;
+using InvestmentManager.DataAccess.EF;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using NLog.Web;
 
 var builder = WebApplication.CreateBuilder(args);
+// NLog 
+NLog.LogManager.LoadConfiguration("nlog.config");
+
+string connectionString = builder.Configuration.GetConnectionString("InvestmentDatabase");
+
+// create an instance of logger factory
+using var loggerFactory = LoggerFactory.Create(loggingBuilder => loggingBuilder
+    .SetMinimumLevel(LogLevel.Trace)
+    // .AddConsole()
+    );
+
 
 // Add services to the container.
+
+// Configure logging
+builder.Services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.AddConsole();
+    loggingBuilder.AddDebug();
+    loggingBuilder.AddNLog();
+});
+
 builder.Services.AddControllersWithViews();
+
+// use the logger factory to create an instance of Ilogger
+ILogger logger = loggerFactory.CreateLogger<Program>();
+builder.Services.RegisterEfDataAccessClasses(connectionString, loggerFactory);
+
+// For Application Services
+string stockIndexServiceUrl = builder.Configuration["StockIndexServiceUrl"];
+builder.Services.AddStockIndexServiceHttpClientWithoutProfiler(stockIndexServiceUrl);
+builder.Services.AddInvestmentManagerServices(stockIndexServiceUrl);
 
 var app = builder.Build();
 
